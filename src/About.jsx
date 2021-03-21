a10374b1120a0e1a9cac76f4aea54d5d343ea586
suyog7101999 <81007959+suyog7101999@users.noreply.github.com>
2021-03-21 17:25:18 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -5,9 +5,16 @@ import web1 from "../src/images/4.jpg";
 const About = () =>{
   return(
     <React.Fragment>
-            <Sidebar name="Welcome to About Page"
-             imgsrc={web1} visit="/contact"
-              btname="Conatct Now" />
+            <Sidebar
+              name="Grow your business with"
+             imgsrc={web}
+             btname="Contact"
+             butname="Service"
+             bttname="About"
+             link="./contact"
+             link1="./service"
+             link2="./about"
+             />
     </React.Fragment>
   );
 };
